6f1a1a858c49150ce721edeab37932e07fc7d278
Vinícius Godoy <vinigodoy@gmail.com>
2018-05-14 15:44:09 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,4 @@
+# Aula 05 - Código final refatorado
+
+Neste repositório criamos classes para o material e a luz. Além disso, incluímos a interface ShaderItem para permitir 
+aplicar objetos complexos diretamente ao Shader. 
---
+++ src/br/pucpr/cg/LitCube.java
@@ -0,0 +1,88 @@
+package br.pucpr.cg;
+
+import br.pucpr.mage.*;
+import br.pucpr.mage.phong.DirectionalLight;
+import br.pucpr.mage.phong.Material;
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+
+public class LitCube implements Scene {
+    private Keyboard keys = Keyboard.getInstance();
+
+    private static final float SPEED = (float) Math.toRadians(180);
+    
+    private Mesh mesh;
+    private float angleX = 0.0f;
+    private float angleY = 0.5f;
+    private Camera camera = new Camera();
+    private DirectionalLight light;
+    private Material material;
+    
+    @Override
+    public void init() {
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_CULL_FACE);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+        mesh = MeshFactory.createCube();
+        camera.getPosition().y = 1.0f;
+
+        light = new DirectionalLight(
+                new Vector3f(1.0f, -1.0f, -1.0f), //Direcao
+                new Vector3f(0.1f, 0.1f, 0.1f),   //Ambient
+                new Vector3f(1.0f, 1.0f, 0.8f),   //Diffuse
+                new Vector3f(1.0f, 1.0f, 1.0f));  //Specular
+
+        material = new Material(
+                new Vector3f(0.5f, 0.0f, 0.5f),   //Ambient
+                new Vector3f(0.5f, 0.0f, 0.5f),   //Diffuse
+                new Vector3f(1.0f, 1.0f, 1.0f),   //Specular
+                32.0f);                           //Specular Power
+    }
+
+    @Override
+    public void update(float secs) {
+        if (keys.isPressed(GLFW_KEY_ESCAPE)) {
+            glfwSetWindowShouldClose(glfwGetCurrentContext(), true);
+            return;
+        }
+
+        if (keys.isDown(GLFW_KEY_A)) {
+            angleY += SPEED * secs;
+        } else if (keys.isDown(GLFW_KEY_D)) {
+            angleY -= SPEED * secs;
+        }
+        
+        if (keys.isDown(GLFW_KEY_W)) {
+            angleX += SPEED * secs;
+        } else if (keys.isDown(GLFW_KEY_S)) {
+            angleX -= SPEED * secs;
+        }
+    }
+
+    @Override
+    public void draw() {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        Shader shader = mesh.getShader();
+        shader.bind()
+                .set(camera)
+                .set(light)
+                .set(material)
+        .unbind();
+
+        mesh.setUniform("uWorld", new Matrix4f().rotateY(angleY).rotateX(angleX));
+        mesh.draw();
+    }
+
+    @Override
+    public void deinit() {
+    }
+
+    public static void main(String[] args) {
+        new Window(new LitCube(), "Cube with lights", 800, 600).show();
+    }
+}
